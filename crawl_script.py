a9362bd642c25af062079e2b07fe94b031825a76
DhongGyuOh <mroh1226@naver.com>
2026-02-16 22:51:32 +0900

    내블로그 크롤링

--- crawl_script.py
+++ crawl_script.py
@@ -1,6 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get('https://www.naver.com/')
-soup = BeautifulSoup(response.text, 'html.parser')
-print(soup.title.string)
+response = requests.get('https://mroh1226.tistory.com/')
+html = response.text
+soup = BeautifulSoup(html, 'lxml')
+print(response.text)
+
+titles = soup.select("strong.tit_post")
+for t in titles:
+    print(t.get_text(strip=True))
+    
+cates = soup.select("a.link_cate")
+for c in cates:
+    print(c.get_text(strip=True))
+
